699ff5076bf071f07f263bdd3125edfc0b8856dc
moazzam-qureshi <qureshimoazzam7@gmail.com>
2025-11-16 23:19:20 +0500

    Fix database URL to use environment variable for Railway

--- database.py
+++ database.py
@@ -2,8 +2,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from typing import Generator
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@postgres:5432/postgres")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
